989c77c3cc3f4dac0bda9b40a2afb65decf1a0e3
Thắng Phan Đức <apgpdt10@gmail.com>
2024-11-19 20:50:05 +0700

    Add project files.

---
+++ WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public static List<Student> StudentList = new List<Student>(); // Danh sách lưu tạm
+
+        [HttpPost]
+        public IActionResult ShowKQ(Student student)
+        {
+            // Lưu sinh viên vào danh sách
+            StudentList.Add(student);
+
+            // Đếm số sinh viên cùng chuyên ngành
+            int count = StudentList.Count(s => s.ChuyenNganh == student.ChuyenNganh);
+
+            // Truyền dữ liệu sang View
+            ViewBag.MSSV = student.MSSV;
+            ViewBag.HoTen = student.HoTen;
+            ViewBag.ChuyenNganh = student.ChuyenNganh;
+            ViewBag.Count = count;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class Student
+    {
+        public string? MSSV { get; set; }
+        public string? HoTen { get; set; }
+        public double? DiemTB { get; set; }
+        public string? ChuyenNganh { get; set; }
+    }
+
+}
